7f0d1e2e19e4bf9cb1dcb52844c7b645a3483e71
Deepali Kundnani <deepali.kundnani@gmail.com>
2021-08-26 12:21:26 -0400

    Update corrs.R

--- R/corrs.R
+++ R/corrs.R
@@ -1,30 +1,36 @@
 #Corr
-library(dplyr)
-library(rlang)
-library(Hmisc)
-library(corrplot)
-library(ggpubr)
-library(MASS)
-library(ggExtra)
-library(GeneNet)
+
 #' Prime Feature Correlation
-#'
 #' Get Correlation Coefficient and its distribution of a single prime feature with all the other features
+#'
+#' @import dplyr
+#' @import rlang
+#' @importFrom Hmisc rcorr
+#' @import corrplot
+#' @import ggpubr
+#' @importFrom MASS kde2d
+#' @import ggExtra
+#' @import GeneNet
+#'
+#'
 #' @param df A dataframe with echo row corresponding to a feature
 #' @param featurelist listing of feature names or ids to measure correlation between
 #' @param primefeature one feature of interest to measure correlation against all other features
+#' @param corrmeth Correlation method used. 'pearson' or 'spearman' (default='pearson')
+#' @param quantiles list of corresponding probabilities for quantiles to be obtained
+#' (default = c(0,0.01,.01,.05,0.10,.25,.50,.75,.90,.95,.99,.999,1) )
 #'
 #' @examples
-#' transdf<- data_transform(df=GTEXv7[,3:ncol(GTEXv7)],transformation='log2', featurelist=GTEXv7[,2], medianthres=0.5)
+#' transdf<- data_transform(df=GTEXv7[-1],transformation='log2', featurelist=GTEXv7$Description,
+#'                          medianthres=0.5)
 #' inputdf<-transdf[[1]] #First item in the list returned is the transformed Data
-#' primefcorr <- primefeature_corr(df=inputdf[,-ncol(inputdf)],featurelist=inputdf$feature ,primefeature="RNASEH2A");
-#' @docType data
+#' primefcorr <- primefeature_corr(df=inputdf,featurelist=rownames(inputdf) ,primefeature="RNASEH2A");
 #'
-#' @usage data(GTEXv7)
+#' @usage primefeature_corr(df,featurelist,primefeature,corrmeth,quantiles)
 #'
 #' @export
 #'
-primefeature_corr <- function(df,featurelist,primefeature,corrmeth='pearson', quantiles=c(0,0.01,.01,.05,0.10,.25,.50,.75,.90,.95,.99,.999,1)){
+primefeature_corr <- function(df,featurelist,primefeature, corrmeth='pearson', quantiles=c(0,0.01,.01,.05,0.10,.25,.50,.75,.90,.95,.99,.999,1)){
   if (length(featurelist)!=nrow(df) && length(primefeature)>1) { stop("Error: Make sure the length of feature list is same as the number of rows in the dataframe and there is only one prime feature.n", call. = FALSE) }
   c=ncol(df)
   r=nrow(df)
@@ -73,25 +79,25 @@ primefeature_corr <- function(df,featurelist,primefeature,corrmeth='pearson', qu
 #' @param df numerical dataframe with rows having series of values for a single feature
 #' @param featurelist list of features in the sequence as it occurs in the Dataframe
 #' @param featuregroup (optional)list of TRUE/FALSE values for features to be observed vs rest of the features(ordering/clustering is disabled)
-#' @param clusterorder Character, the ordering method of the correlation matrix.'original' for original order (default),'AOE' for the angular order of the eigenvectors, 'FPC' for the first principal component order, 'hclust' for the hierarchical clustering order, 'alphabet' for alphabetical order.
-#' @param clustermethod Character, the ordering method of the correlation matrix.'original' for original order (default),'AOE' for the angular order of the eigenvectors, 'FPC' for the first principal component order, 'hclust' for the hierarchical clustering order, 'alphabet' for alphabetical order.
+#' @param visorder  The ordering method of the correlation matrix.'original' for original order ,'AOE' for the angular order of the eigenvectors, 'FPC' for the first principal component order, 'hclust'(default) for the hierarchical clustering order, 'alphabet' for alphabetical order.
+#' @param corrmeth Correlation method used. 'pearson' or 'spearman' (default='pearson')
 #'
 #' @examples
 #' pairwisedf<-TCGA40[,3:ncol(TCGA40)]
-#' corr_pair<-pairwise_corr(df=pairwisedf,featurelist=rownames(pairwisedf), clusterorder="hclust")
-#' featgroup<-grepl( "RNASE",rownames(pairwisedf)) #optional, a set of features to separated
-#' corr_pair<-pairwisecorr <- pairwise_corr(df=pairwisedf,featurelist=rownames(pairwisedf), featuregroup=featgroup);
-#' @docType data
+#' corr_pair<-pairwise_corr(df=logTCGA40,featurelist=rownames(logTCGA40), visorder="hclust")
+#' featgroup<-grepl( "RNASE",rownames(logTCGA40)) #optional, a set of features to separated
+#' corr_pair<-pairwisecorr <- pairwise_corr(df=pairwisedf,featurelist=rownames(pairwisedf),
+#'                                        featuregroup=featgroup)
 #'
-#' @usage data(TCGA40)
+#' @usage pairwise_corr(df,featurelist, featuregroup,visorder,corrmeth)
 #'
 #' @export
-pairwise_corr <- function(df,featurelist, featuregroup='NA', clusterorder="hclust"){
+pairwise_corr <- function(df,featurelist, featuregroup='NA', visorder="hclust", corrmeth='pearson'){
   if (length(featurelist)!=nrow(df)) { stop("Error: Make sure the length of feature list is same as the number of rows in the dataframe", call. = FALSE) }
   rownames(df)<-featurelist
-  corr_pair<-rcorr(t(as.matrix(df)))
+  corr_pair<-rcorr(t(as.matrix(df)), type=corrmeth)
   par(mfrow=c(1, 1), mar=c(1,4,4,8))
-  if (featuregroup == 'NA') {
+  if (length(featuregroup) == 1) {
     corrplot(corr_pair$r,type = "full", order =  "hclust",addrect = 2,method = "square",tl.col = "black",tl.srt = 45, cl.cex = 1.3, pch.cex = 0.5)
     out=list(corr_pair$r,corr_pair$P)
   } else {
@@ -103,28 +109,35 @@ pairwise_corr <- function(df,featurelist, featuregroup='NA', clusterorder="hclus
 }
 
 #' Pair Scatter
-#'
 #' Scatter between two chosen features.
+#'
+#' @import ggpubr
+#' @import ggplot2
+#'
+#'
 #' @param df numerical dataframe with rows having series of values for a single feature
 #' @param featurelist list of features in the sequence as it occurs in the Dataframe
 #' @param feature1,feature2 Features to be compared
+#' @param corrmeth Correlation method used. 'pearson' or 'spearman' (default='pearson')
 #'
-#' @docType data
 #'
-#' @usage data(TCGA40)
+#' @usage pair_scatter(df,featurelist, feature1, feature2, corrmeth)
 #'
 #' @examples
-#' pscatter <- pair_scatter(df=inputdf[,-ncol(inputdf)],featurelist=inputdf$feature,feature1="RNASEH2A",feature2="RNASEH2C");
-#' pscatter <- pair_scatter(df=TCGA40,featurelist=rownames(TCGA40), feature1="RNASEH2A", feature2="RNASEH2C");
+#' pscatter <- pair_scatter(df=TCGA40,featurelist=rownames(TCGA40),
+#'                         feature1="RNASEH2A",feature2="PCNA", corrmeth='pearson')
+#' pscatter <- pair_scatter(df=logTCGA40,featurelist=rownames(logTCGA40),
+#'                         feature1="RNASEH2A", feature2="PCNA", corrmeth='pearson')
+#'
 #' @export
-pair_scatter <- function(df,featurelist, feature1, feature2){
-  if (length(featurelist)!=nrow(df)) { stop("Error: Make sure the length of feature list is same as the number of rows in the dataframe and there is only one prime feature.n", call. = FALSE) }
+pair_scatter <- function(df,featurelist, feature1, feature2,corrmeth='pearson'){
+  if (length(featurelist)!=nrow(df)) { stop("Error: Make sure the length of feature list is same as the number of rows in the dataframe", call. = FALSE) }
 
   data=as.data.frame(t(df))
   colnames(data)<-featurelist
-  #scatter plot with Pearson's R
+  #scatter plot
   par(mfrow = c(1, 1))
-  p<-ggscatter(data, x = feature1 , y = feature2, color = "#2c7fb8", margin.params = list(fill = "lightgray"), add = "reg.line", add.params = list(color='red', fille='lightgray'),conf.int = TRUE, cor.coef = TRUE, cor.coeff.args = list(method='pearson',label.x.npc = 0,label.y.npc = 1, size=10)) +theme_bw()
+  p<- ggscatter(data, x = feature1 , y = feature2, color = "#2c7fb8", margin.params = list(fill = "lightgray"), add = "reg.line", add.params = list(color='red', fille='lightgray'),conf.int = TRUE, cor.coef = TRUE, cor.coeff.args = list(method=corrmeth,label.x.npc = 0,label.y.npc = 1, size=10)) +theme_bw()
   ggMarginal(p, size = 2, type = "histogram", col = "blue", fill = "#2c7fb8")
   p <- recordPlot()
 
@@ -138,19 +151,6 @@ pair_scatter <- function(df,featurelist, feature1, feature2){
   q
   return(list(p,q))
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
